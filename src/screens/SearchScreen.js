434bbb53740ef2ae9bf040e32ff410a8fdad9220
usman133 <usmankiani26@gmail.com>
2023-04-05 15:00:38 +1200

    SearchScreen

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, StyleSheet } from 'react-native';
+
+const SearchScreen = () => {
+    return <View>
+        <Text>Search screen</Text>
+    </View>
+}
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
