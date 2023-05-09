e2fe2e2130d4871622755416756753578a092159
usman133 <usmankiani26@gmail.com>
2023-05-09 18:59:33 +1200

    Error msg

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -6,29 +6,36 @@ import yelp from "../api/yelp";
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
     const [results, setResults] = useState([]);
+    const [errorMessage, setErrroMessage] = useState('');
 
     const searchApi = async () => {
-      const response = await yelp.get('/search', {
-        params: {
-           limit: 50,
-           term,
-           location: 'San Jose'
+      try {
+        const response = await yelp.get('/search', {
+          params: {
+            limit: 50,
+            term,
+            location: 'San Jose'
         }
       })
 
       setResults(response.data.businesses);
+    } catch (err) {
+      setErrroMessage('Something went wrong');
+
     }
-
-
-    return <View>
+  }
+  
+    return (
+    <View>
         <SearchBar 
         term={term}
         onTermChange={setTerm}
         onTermSubmit={searchApi}/>
-        <Text>Search screen</Text>
+        {errorMessage ? <Text>{errorMessage}</Text> : null}
         <Text>We have found {results.length} results</Text>
         <Text>{term}</Text>
     </View>
+    )
 }
 
 const styles = StyleSheet.create({});
