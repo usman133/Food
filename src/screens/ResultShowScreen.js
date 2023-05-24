3ed82d5f66d297cbe8fb986f511c995902de8c19
usman133 <usmankiani26@gmail.com>
2023-05-24 22:47:21 +1200

    ResultShowScreen

---
+++ src/screens/ResultShowScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from 'react-native';
+
+const ResultShowScreen = () => {
+    return (
+        <View>
+            <Text>Results show</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({});
+
+export default ResultShowScreen;
