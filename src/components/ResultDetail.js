6cee19a5aece78f5538305c5439867c62d9c69c8
usman133 <usmankiani26@gmail.com>
2023-05-24 22:09:53 +1200

    Adding styles

--- src/components/ResultDetail.js
+++ src/components/ResultDetail.js
@@ -3,18 +3,27 @@ import { View, Image, Text, StyleSheet, FlatList } from 'react-native';
 
 const ResultDetail = ({ result }) => {
     return (
-    <View>
+    <View style={styles.container}>
         <Image style={styles.image} source={{uri: result.image_url}} />
-        <Text>{result.name}</Text>
+        <Text style={styles.name}>{result.name}</Text>
+        <Text>{result.rating} Stars, {result.review_count} Reviews</Text>
     </View>
     )
 }
 
 const styles = StyleSheet.create({
+    container: {
+        marginLeft:15
+    },
     image: {
         width:250,
         borderRadius:4,
-        height:120
+        height:120,
+        marginBottom:5
+    },
+    name: {
+        fontWeight: 'bold',
+        
     }
 });
 
