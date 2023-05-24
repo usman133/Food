f17202b330e82beb1642f4f10cb0db722eeadb98
usman133 <usmankiani26@gmail.com>
2023-05-24 21:24:55 +1200

    Displaying names of restaurants, filteted by price

--- src/components/ResultList.js
+++ src/components/ResultList.js
@@ -1,14 +1,27 @@
 import React from "react";
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 
-const ResultList = () => {
+const ResultList = ( { title, results }) => {
     return (
         <View>
-            <Text>ResultList</Text>
+            <Text style={styles.title}>{title}</Text>
+            <FlatList
+               horizontal
+               data={results}
+               keyExtractor={(result) => result.id}
+               renderItem={({ item }) => {
+                return <Text> {item.name} </Text>
+               }}
+            />
         </View>
     )
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 18,
+        fontWeight: 'bold'
+    }
+});
 
 export default ResultList;
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,11 +1,19 @@
-import React, { useState, useEffect  } from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from "../components/SearchBar";
 import useResults from "../hooks/useResults";
+import ResultList from "../components/ResultList";
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
     const [searchApi, results, errorMessage] = useResults();
+
+    const filterResultsByPrice = (price) => {
+        // price === '$' || '$$' || '$$$' || '$$$$'
+        return results.filter(result => {
+            return result.price === price;
+        })
+    }
 
     return (
     <View>
@@ -16,7 +24,9 @@ const SearchScreen = () => {
         />
         {errorMessage ? <Text>{errorMessage}</Text> : null}
         <Text>We have found {results.length} results</Text>
-        <Text>{term}</Text>
+        <ResultList results={filterResultsByPrice('$')} title="Cost Effective" />
+        <ResultList results={filterResultsByPrice('$$')} title="Bit Pricier" />
+        <ResultList results={filterResultsByPrice('$$$')} title="Big Spender" />
     </View>
     )
 }
