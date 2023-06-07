8f3e44b288b95e4dcbcb6c7202fb7ef1b0aacd67
usman133 <usmankiani26@gmail.com>
2023-06-08 11:29:55 +1200

    Hide empty sections

--- src/components/ResultList.js
+++ src/components/ResultList.js
@@ -4,6 +4,10 @@ import ResultDetail from "./ResultDetail";
 import { withNavigation } from "react-navigation";
 
 const ResultList = ( { title, results, navigation }) => {
+    if (!results.length) {
+        return null;
+    }
+    
     return (
         <View style={styles.container}>
             <Text style={styles.title}>{title}</Text>
